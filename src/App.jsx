b829ed0d044eb768f97d64c2109128770d0d5a80
Marian <hellomariandev@gmail.com>
2023-07-08 22:12:34 +0200

    set up

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-[100vh] flex flex-row items-center justify-center bg-slate-300">
+      <div className="p-6 bg-slate-100 rounded-xl shadow-md">
+        <h2 className="text-2xl font-bold underline">Haiku Poems</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
